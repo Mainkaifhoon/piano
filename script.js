42d0afaa8c9d0c1f21c4a5ce10dca0508d1e745a
Mainkaifhoon <pnmkaif@gmail.com>
2026-02-10 01:52:55 +0530

    First version

---
+++ script.js
@@ -0,0 +1,39 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheckbox= document.querySelector(".keys-checkbox input")
+
+let allKeys = [];
+let audio = new Audio("/tunes/a.wav")
+
+const playTune = (key)=>{
+    audio.src=`/tunes/${key}.wav`
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add("active");
+    setTimeout(()=>{
+        clickedKey.classList.remove("active")
+    }, 250)
+}
+pianoKeys.forEach(key=>{
+    allKeys.push(key.dataset.key)
+    key.addEventListener("click",()=>playTune(key.dataset.key))
+});
+
+const pressedKey=(e)=>{
+
+   if(allKeys.includes(e.key)) playTune(e.key)
+
+};
+
+const showHideKeys=()=>{
+    pianoKeys.forEach(key=>key.classList.toggle("hide"))
+}
+
+const handleVolume = (e)=>{
+    audio.volume = e.target.value
+}
+
+volumeSlider.addEventListener('input', handleVolume)
+document.addEventListener('keydown', pressedKey)
+keysCheckbox.addEventListener('click',showHideKeys)
